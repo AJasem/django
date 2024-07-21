91ee318ccea354758780330c1bc73befcdaf5fdd
AJasem <ajassem.ahmad@gmail.com>
2024-07-21 23:20:07 +0200

    register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-      # register user
-      messages.error(request,'testing error')
-      return redirect('register')
+    #   get form values
+     first_name = request.POST['first_name']
+     last_name = request.POST['last_name']
+     username = request.POST['username']
+     email = request.POST['email']
+     password = request.POST['password']
+     password2 = request.POST['password2']
+    #  check password match
+     if password == password2:
+    #    check username
+       if User.objects.filter(username=username).exists():
+          messages.error(request,'Username is already taken')
+          return redirect('register')
+       else:
+          if User.objects.filter(email=email).exists():
+             messages.error(request,'Email already exists')
+             return redirect('register')
+          else:
+            #  ok
+            user = User.objects.create_user(username=username,
+                                            password=password,email=email,
+                                            first_name=first_name,last_name=last_name)
+            
+            user.save()
+            messages.success(request,'Account created successfully!')
+            return redirect('login')
+          
+       
+     else:
+        messages.error(request,'Passwords do not match')
+        return redirect('register')
     else:
      return render(request,'accounts/register.html')
 
