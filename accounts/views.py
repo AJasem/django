ec6719521a0abe69222fa75ad110a5a918c580a9
AJasem <ajassem.ahmad@gmail.com>
2024-07-22 02:15:14 +0200

    login and dashboard and dyin title

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
@@ -40,12 +40,26 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-    #    login user
-       return
+       username = request.POST['username']
+       password = request.POST['password']
+       user = auth.authenticate(username=username,password=password)
+       if user is not None:
+          auth.login(request,user)
+          messages.success(request,'You are logged in')
+          return redirect('dashboard')
+       else:
+          messages.error(request,'Invalid Credentials')
+          return redirect('login')
+
     else:
      return render(request,'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+       auth.logout(request)
+       messages.success(request,'Logged out!')
+       return redirect('index')
+    
     return redirect('index')
 
 def dashboard(request):
