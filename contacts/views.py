ddc71dd9216a4bbdb00f34b5589642a28527f878
AJasem <ajassem.ahmad@gmail.com>
2024-07-23 13:59:56 +0200

    form submit submit only once

--- contacts/views.py
+++ contacts/views.py
@@ -11,10 +11,19 @@ def contact(request):
             listing = request.POST.get('listing', '')
             name = request.POST['name']
             email = request.POST['email']
-            phone = request.POST.get('phone', '')
-            message = request.POST.get('message', '')
-            user_id = request.POST.get('user_id', None)
-            realtor_email = request.POST.get('realtor_email', '')
+            phone = request.POST['phone']
+            message = request.POST['message']
+            user_id = request.POST['user_id']
+            realtor_email = request.POST['realtor_email']
+            # checkif user has made inquiry already
+            if request.user.is_authenticated:
+                  user_id = request.user.id
+                      
+                  has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+                  if has_contacted:
+                        messages.error(request,'You have already made an inquiry for this listing')
+                        return redirect('/listings/'+ listing_id)
+                  
 
             contact = Contact(
                 listing=listing,
